bd1509216803363e4339c6487b33458a864a03e5
Amrutha Manjunath <amruthamanjunath777@gmail.com>
2025-10-04 15:43:09 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+int num;
+printf("enter your number: ");
+scanf("%d",&num);
+if(num>0){
+   printf("it is natural number");
+} else {
+   printf("not a natural number");
+}
+return 0;
+}
+
+
